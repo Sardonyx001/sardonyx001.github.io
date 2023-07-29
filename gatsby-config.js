c1930213e574a9dc6399208f2965b4931649106b
Sardonyx001 <lassouedjamel@hotmail.fr>
2023-07-29 15:24:09 +0900

    Updated website icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,7 +102,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "RSS Feed",
           },
         ],
       },
@@ -118,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/userpic.png`, // This path is relative to the root of the site.
       },
     },
   ],
